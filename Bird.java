482aef8be4b5730fbbbc5a5ca1e1167706624a76
SOWJANYA <sajibillisowjanya1@gmail.com>
2025-05-20 08:46:26 +0530

    Update Bird.java

--- Bird.java
+++ Bird.java
@@ -1,1 +1,35 @@
+package Birds;
+
+public class Bird {
+    private int weight;
+    private String name;
+    private String color;
+
+    public void setColor(String newColor) {
+              this.color = newColor;
+    }
+
+    public void setWeight(int newWeight) {
+             this.weight = newWeight;
+    }
+
+    public void setName(String newName) {
+             this.name = newName;
+    }
+
+
+    public String getColor(String color) {
+        return color;
+    }
+
+    public String getName(String name){
+        return name;
+    }
+
+    public int getWeight(int weight){
+        return weight;
+    }
+}
+
+
 
