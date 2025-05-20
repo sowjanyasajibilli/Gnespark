e167867b06bfeb84c1e8e617946f417898b813f6
SOWJANYA <sajibillisowjanya1@gmail.com>
2025-05-20 09:55:43 +0530

    Create MergeSort.java

---
+++ MergeSort.java
@@ -0,0 +1,69 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class Merge_Sort_Algo {
+
+    public static void mergeSort(int[] ar, int low, int high) {
+        if (low < high) {                       //Base case - checking if array is empty or single valued.
+            int mid = (low + high) / 2;
+            mergeSort(ar, low, mid);                  // Left Sub array
+            mergeSort(ar, mid + 1, high);         // Right sub array
+
+            mergebothhalfs(ar,low,mid,high);
+        }
+    }
+
+    public  static void mergebothhalfs(int arr[], int left, int middle, int right) {
+        int[] temp = new int[right - left + 1]; // temp array to store merged result
+        int i = left;              // Initial index of left subarray
+        int j = middle + 1;        // Initial index of right subarray
+        int k = 0;                 // Initial index of merged array
+
+        while (i <= middle && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        // Copy remaining elements of left subarray if any
+        while (i <= middle) {
+            temp[k++] = arr[i++];
+        }
+
+        // Copy remaining elements of right subarray if any
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        // Copy merged elements back to original array
+        for (i = 0; i < temp.length; i++) {
+            arr[left +i] = temp[i];
+        }
+    }
+
+
+    public static void printarray(int[] ar,int n){
+        for(int i = 0 ; i < n ; i++){                        // Function to print array
+            System.out.print(ar[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr  = new int[10];
+        for (int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array Elements before sorting : ");
+        printarray(arr,n);
+        mergeSort(arr,0,n-1);
+        System.out.println("Array Elements after sorting : ");
+        printarray(arr,n);
+    }
+
+}
