4c2f3dfb54dbc95f0e21f7db4d2f2ecdc6d46755
SOWJANYA <sajibillisowjanya1@gmail.com>
2025-05-20 08:50:27 +0530

    Create Eagle.java

---
+++ Eagle.java
@@ -0,0 +1,13 @@
+package Birds;
+
+public class Eagle extends Bird implements Eat,Fly,SaySomething{
+    public void eat(){
+
+    }
+    public void fly(){
+
+    }
+    public void saySomething(){
+
+    }
+}
