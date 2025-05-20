7f2ebfb830b0176548b3ec315c7027b130defd0e
SOWJANYA <sajibillisowjanya1@gmail.com>
2025-05-20 15:50:38 +0530

    Update Recurrsion-part2.java

--- Recurrsion-part2.java
+++ Recurrsion-part2.java
@@ -1,3 +1,146 @@
+1.Print the numbers in descending order using recursion
+
+
+Public void printseriesdsc(int n){
+
+  If (n == 0) {
+   System.out.print(n);                    // base case (if the series ends with 0)
+    Return;
+}
+
+  If (n == 1) {                                    //base case (if the series ends with 1)
+    System.out.print(n);
+    Return; }
+
+ Else {
+   System.out.print(n);
+   return printseriesdsc(n-1)           //  other than base case
+}
+
+inp : 7
+out : 7 6 5 4 3 2 1
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+2. Print the numbers in ascending-order using recursion
+
+Public void printNumberAsc(int n){
+     Int i == 0;            // if series starts from 0
+      If ( i < = n){
+      System.out.println(i) ;
+       i++;
+      Return printNumberAsc(i);          // performing recursion 
+}
+}
+
+Inp : 7
+out : 1 2 3 4 5 6 7
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+3. Both ascending and descending
+
+Public static int orderprint(int n){
+  if (n == 0) {   		//base case
+        Return;              
+    } 
+else {
+        orderprint(n - 1);   	// for   ascending order
+        System.out.print(“ “ + n);  
+   }
+    System.out.print(n + " ");   }    // for descending order
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+4.  Print the pattern using recursion
+
+******
+****
+***
+**
+*
+
+Public int printpattern(int n){
+If  (n==1){ 					// base case
+System.out.println(“*”);
+Return 0;
+}
+Else {
+System.out.println(“*”.repeat(n));       		// prints n number of stars
+Return printpattern(n-1);
+}
+}
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+5. Print the pattern using recursion
+*
+**
+****
+*****
+******
+
+Public static int printpattern(int n, int i){
+If (i>n) return 0;			// base case  and i starts from 1
+Else{
+System.out.println(“*”.repeat(i));
+I++;
+}
+Return printpattern(n,i);
+}
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+6. Print the pattern using recursion
+
+1
+1 2
+1 2 3 
+1 2 3 4
+1 2 3 4 5
+
+
+
+Public static int numberpattern(int r,int i){
+    If ( i > r ) return 0				//base case,, i starts from 1 
+    Else {
+     For (int j = 1; j<=1 ; j++) {
+           System.out.println(“ “ + j);  	// prints the number series
+}	
+   I ++;              // for next row
+Return numberpattern(r,i)		//recursive call for other until “i” exceeds rows.
+}
+}
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+BONUS PROBLEMS :
+
+1.Reverse the linkedlist using recursion
+
+Node reverselinkedlist( Node head) {
+If (head == null || head.next == null) {
+Return head; }
+ Node newHead = revrselinkedlist(head.next);
+Head.next.next = head;
+Head.next = null;
+Return newHead;
+}
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+2.Reverse the array using recursion
+
+Public static int printrevarray(int[] arr, int n){
+ If (n<=0) return 0 ;
+Else {
+System.out.println(“ “+arr[n-1]);
+Return printrevarray(arr, n-1);
+}
+
+Inp : arr = 1 2 3 4 5 
+out : arr = 5 4 3 2 1
+    
 Recursion : A Function calling itself is called Recursion.
 
 1.  Factorial Problem :
